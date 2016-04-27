b953b2ca420ecaa566814cf93a8c09af50744995
Lucas Rencoret <lucasrencoret36@gmail.com>
2016-04-27 11:22:47 -0300

    metodo encriptar

--- app/models/bodega.rb
+++ app/models/bodega.rb
@@ -1,2 +1,19 @@
+require 'rubygems'
+require 'base64'
+require 'cgi'
+require 'hmac-sha1'
+
 class Bodega < ActiveRecord::Base
+
+def encriptar(string texto)
+    key = 'WqhY79mm3N4ph6'
+    signature = texto
+    hmac = HMAC::SHA1.new(key)
+    hmac.update(signature)
+    puts CGI.escape(Base64.encode64("#{hmac.digest}\n"))
+    return hmac
+end
+def obtener_almacenes()
+
+
 end
