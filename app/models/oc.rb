9db5614018948c187fd02b9d9ca97f2075b0274a
pfsamaja <pfsamaja@uc.cl>
2016-05-01 18:20:11 -0300

    gemfile actualizado

--- app/models/oc.rb
+++ app/models/oc.rb
@@ -1,2 +1,9 @@
+require 'open-uri'
+require 'rubygems'
 class Oc < ActiveRecord::Base
+def self.getOc(id)
+    buffer = open('http://mare.ing.puc.cl/oc/obtener/'+id , "Content-Type"=>"application/json").read
+    resultado = JSON.parse(buffer)
+    
+end
 end
