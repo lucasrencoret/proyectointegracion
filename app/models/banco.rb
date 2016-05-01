6cc3e054fcafd012dc7170e31c2a45cb3a7777a7
pfsamaja <pfsamaja@uc.cl>
2016-05-01 17:42:26 -0300

    metodo obtener cuenta

--- app/models/banco.rb
+++ app/models/banco.rb
@@ -1,2 +1,10 @@
+require 'open-uri'
+require 'rubygems'
+
 class Banco < ActiveRecord::Base
+def self.obtenerCuenta(cuentaId)
+	buffer = open('http://mare.ing.puc.cl/banco/cuenta/'+cuentaId , "Content-Type"=>"application/json").read
+	resultado = JSON.parse(buffer)
+
+end
 end
