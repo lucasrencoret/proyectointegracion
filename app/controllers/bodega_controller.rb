29f2fce7a9073bba60474afdfaecd1b40319f4b5
Lucas Rencoret <lucasrencoret36@gmail.com>
2016-05-02 18:47:52 -0300

    render json de consultar

--- app/controllers/bodega_controller.rb
+++ app/controllers/bodega_controller.rb
@@ -3,8 +3,8 @@ class BodegaController < ApplicationController
      
     end
     def consultar
-        
         response = Bodega.consultar(params[:sku])
+        render :json => response
     end
     
 end
