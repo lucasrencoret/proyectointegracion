84720b57dedbfe129827f44b67c4021e87b6522e
crosende1 <crosende1@uc.cl>
2016-05-02 20:15:18 -0300

    Crearboleta

--- app/models/factura.rb
+++ app/models/factura.rb
@@ -29,4 +29,12 @@ def self.anularFactura(iD,motiv)
 	
 end
 
+def self.crearBoleta(proveed, cliente, total)
+	
+	RestClient.put 'http://mare.ing.puc.cl/facturas/boleta', {:proveedor => proveed, :cliente => cliente, :total => total}.to_json, :content_type=> 'application/json'
+	
+end
+
+
+
 end
