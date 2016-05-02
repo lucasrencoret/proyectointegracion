aa7d52429b2ed4727e33e69677eefb0eaefc29c1
pfsamaja <pfsamaja@uc.cl>
2016-05-02 14:53:23 -0300

    oc actualizado

--- app/models/oc.rb
+++ app/models/oc.rb
@@ -12,9 +12,13 @@ def self.recepcionarOc(iD)
 	RestClient.post 'http://mare.ing.puc.cl/oc/recepcionar/'+iD, {:id => iD}.to_json, :content_type=> 'application/json'
 	
 end
-def self.crearOc(cAnal,Cantidad,sKu,pRoveedor,pRecioUnitario,nOtas,cLiente,fEchaEntrega)
+def self.rechazarOc(iD,motivo)
+	RestClient.post 'http://mare.ing.puc.cl/oc/rechazar/'+iD, {:rechazo =>motivo}.to_json, :content_type=> 'application/json'
 	
-	RestClient.put 'http://mare.ing.puc.cl/oc/crear', {:canal => cAnal, :cantidad => cAntidad, :sku => sKu, :proveedor => pRoveedor, :precioUnitario => pRecioUnitario, :notas => nOtas, :cliente => cLiente, :fechaEntrega => fEchaEntrega}.to_json, :content_type=> 'application/json'
+end
+def self.crearOc(cAnal, cAntidad, skuid, pRoveedor, pRecioUnitario, nOtas, cLiente, fEchaEntrega)
+	
+	RestClient.put 'http://mare.ing.puc.cl/oc/crear', {:canal => cAnal, :cantidad => cAntidad, :sku => skuid, :proveedor => pRoveedor, :precioUnitario => pRecioUnitario, :notas => nOtas, :cliente => cLiente, :fechaEntrega => fEchaEntrega}.to_json, :content_type=> 'application/json'
 	
 end
 end
