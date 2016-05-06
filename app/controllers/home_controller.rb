0d67dd5d5cc596de08c3fee49ac2ac08d6c9ae4b
crosende1 <crosende1@uc.cl>
2016-05-06 20:43:38 -0300

    cata

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 
 class HomeController < ApplicationController
   def index
-  
+  	@almacenes = Bodega.getAlmacenes()
+  	#@facturas= Facturas.obtenerFactura(:id => params[:iD])
+
+  	# <%= Factura.find_by(:id => params[:iD])%>
+
   end
   
   
