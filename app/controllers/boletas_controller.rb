d59746f733a56f182794e845fc728c3801d4053a
Lucas Rencoret <lucasrencoret36@gmail.com>
2016-06-03 19:54:19 -0300

    ok view

--- app/controllers/boletas_controller.rb
+++ app/controllers/boletas_controller.rb
@@ -1,5 +1,20 @@
 class BoletasController < ApplicationController
   def ok
+    id_boleta = params[:_id]
+    @boleta = B2c.find_by _id: id_boleta
+    
+    #MANDAR A DESPACHAR
+    direccion = @boleta.direccion
+    sku = @boleta.sku
+    cantidad = @boleta.cantidad
+    id_boleta = @boleta._id
+    total_plata = @boleta.total
+    iva = @boleta.iva
+    bruto = @boleta.bruto
+    proveedor = @boleta.proveedor
+    cliente = @boleta.cliente
+    
+    
   end
 
   def fail
