fad926c4b0d350b5658f3031cd7995fb35c4efd6
crosende1 <crosende1@uc.cl>
2016-06-28 21:05:24 -0400

    publicar fbk

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -2,7 +2,8 @@ class TwitterController < ApplicationController
   def index
   end
 
-  def publish
+  def publicar_facebook(sku, precio, inicio, fin, codigo, publicar)
+  	texto= "Utiliza el codigo #{codigo} para tener un descuento en el producto #{sku} a tan solo el precio de #{precio}. Apurate! Valido desde #{inicio} hasta #{fin}"
   	client = Twitter::REST::Client.new do |config|
   config.consumer_key        = "LRXI8ilvCl0Hss49OcUvhzjgS"
   config.consumer_secret     = "Ve6NTOjYFqpgPOOwAXAqkIhzqd64Ss8rm7nJSlekhiR3LQLHkF"
@@ -15,5 +16,8 @@ end
 
   @graph = Koala::Facebook::API.new("EAAOZBw81vhr8BALLLv09Vck2t5DqX92YZC3RFjbewiV4WwHly1PUPZA8ENV1IyIdUWAsmww4MEwFU1r7lPxpHno28TRylBDSFYRZBZCMeMWjUAnfFSJ236MxcuUluIaNgPXR1Cs3hh72LzmnXoViV2XLHFQxqxkTtleccWXJTnAZDZD")
   @graph.put_wall_post("text",{link:"http://e03-elmundo.uecdn.es/assets/multimedia/imagenes/2015/11/13/14474300157302.jpg"})
+
+
+  publicar_facebook(sku, precio, inicio, fin, codigo, publicar)
 
 end
