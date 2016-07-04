add500c7c70668bc00c09cf978b38e195accb508
crosende1 <crosende1@uc.cl>
2016-07-04 15:32:37 -0400

    cambios

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -2,18 +2,21 @@ class TwitterController < ApplicationController
   def index
   end
 
-  def publish
-  	client = Twitter::REST::Client.new do |config|
+  def publish (sku, precio, inicio, fin, codigo, publicar)
+
+if sku=='20'
+  client = Twitter::REST::Client.new do |config|
   config.consumer_key        = "LRXI8ilvCl0Hss49OcUvhzjgS"
   config.consumer_secret     = "Ve6NTOjYFqpgPOOwAXAqkIhzqd64Ss8rm7nJSlekhiR3LQLHkF"
   config.access_token        = "747885503983280128-aQUz1SOHqGvvsSBH3jqPXOtYsd9AkHV"
   config.access_token_secret = "XleArA7mRLB1HuQHmmN5XAYndNzke7E9fYqQeBEnsdypi"
 end
 
-	client.update_with_media("text", open("http://e03-elmundo.uecdn.es/assets/multimedia/imagenes/2015/11/13/14474300157302.jpg"))
+	client.update_with_media("text", open("http://www.chocozona.com/files/image/0/666/4f16ee2ed5421.jpg"))
   end
 
   @graph = Koala::Facebook::API.new("EAAOZBw81vhr8BALLLv09Vck2t5DqX92YZC3RFjbewiV4WwHly1PUPZA8ENV1IyIdUWAsmww4MEwFU1r7lPxpHno28TRylBDSFYRZBZCMeMWjUAnfFSJ236MxcuUluIaNgPXR1Cs3hh72LzmnXoViV2XLHFQxqxkTtleccWXJTnAZDZD")
-  @graph.put_wall_post("text",{link:"http://e03-elmundo.uecdn.es/assets/multimedia/imagenes/2015/11/13/14474300157302.jpg"})
+  @graph.put_wall_post("text",{link:"http://www.chocozona.com/files/image/0/666/4f16ee2ed5421.jpg"})
 
 end
+end 
--- app/models/promo.rb
+++ app/models/promo.rb
@@ -16,58 +16,40 @@ def self.leer_cola
 	ch = b.create_channel
 
   # declare a queue
-	q = ch.queue("codigos") 
+	q = ch.queue('ofertas',:auto_delete => true, :exclusive => false, :durable => false) 
 
   # get message from the queue 
-  promos = []
+  promo = q.pop
+  promocion=promo[2].to_s
+  prom=JSON.parse(promocion)
 
-	delivery_info, metadata, payload = q.pop
-
-  while payload != nil
-    promos << JSON.parse(payload)
-    delivery_info, metadata, payload = q.pop
+  sku= prom['sku']
+  precio=prom['precio']
+  inicio=prom['inicio']
+  fin=prom['fin']
+  codigo=prom['codigo']
+  publicar=prom['publicar']
+  if publicar
+    Promo.create(sku: sku, precio: precio, inicio: inicio, fin: fin, codigo: codigo)
+    publish(sku,precio,inicio,fin,codigo,publicar)
   end
-
   # close the connection
-	b.stop
-  promos
+	ch.close
+  b.stop
 end
 
 def self.cronjob
-	promos = leer_cola
+	leer_cola
 	return nil if promos.empty?
 
   promos.each do |h|
 	 sku, precio, inicio, fin, codigo, publicar = h["sku"], h["precio"], Time.at(h["inicio"].to_i / 1000), Time.at(h["fin"].to_i / 1000), h["codigo"], h["publicar"]
 	  # publicar_facebook(sku, precio, inicio, fin, codigo, publicar) if publicar
 	  # publicar_twitter(sku, precio, inicio, fin, codigo, publicar) if publicar
-    Promo.create!(sku: sku, precio: precio, inicio: inicio, fin: fin, codigo: codigo)
+    
   end 
 end
 
-def self.test_publicar
-  b = Bunny.new 'amqp://ioxyydey:ARS_RdIo2M2cntXkecPQcSCTLvMwiwxV@hyena.rmq.cloudamqp.com/ioxyydey'
-  b.start
 
-  # open a channel
-  ch = b.create_channel
-
-  # declare a queue
-  q = ch.queue("codigos") 
-
-  # publish a message to the exchange which then gets routed to the queue
-  3.times do
-    q.publish('{
-    "sku" : "32987124",
-    "precio" : 1999,
-    "inicio" : "1700000000000",
-    "fin" : "1710000000000",
-    "codigo": "mi_codigo",
-    "publicar": true
-    }')
-  end
-  b.stop
-
-end
 
 end
